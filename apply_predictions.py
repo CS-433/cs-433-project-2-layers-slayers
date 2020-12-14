6d3b80ff0080a3e3e8e7bb3d35814267bb4b12f9
Cyrilvallez <71554963+Cyrilvallez@users.noreply.github.com>
2020-12-14 11:54:15 +0100

    changes in apply_prediction

--- apply_predictions.py
+++ apply_predictions.py
@@ -22,26 +22,25 @@ imgs = Imaging.load_test().cuda()
 print(imgs.shape)
 
 preds = torch.zeros(imgs.shape[0],imgs.shape[2],imgs.shape[3])
-num_subgroups = 50
-range_subgroup = imgs.shape[0]//num_subgroups
-for i in range(num_subgroups-1):
-  preds[range_subgroup*i:range_subgroup*(i+1)] = ML.validation.get_prediction(model(imgs[range_subgroup*i:range_subgroup*(i+1)]), False)
-print(preds.shape)
 
+for i in range(imgs.shape[0]):
+  out = model(imgs[i:i+1])
+  pred = ML.validation.get_prediction(out, True)
+  pred = pred.view(pred.shape[1], pred.shape[2])
+  preds[i] = pred
+
+## Optionnaly save the predictions -------------------------------------------
 test_dir = "data/test_set_images/"
 N = 50
 files = ['test_' + '%d' %i for i in range(1,N+1)]
 for i in range(len(files)):
   Imaging.save_img(preds[i],files[i],'output/')
-  
+##----------------------------------------------------------------------------
+
 
 submission_filename = 'first_try.csv'
-image_filenames = []
-for i in range(1,51):
-  image_filename = 'output/test_' + '%d' % i + '.png'
-  image_filenames.append(image_filename)
 
-helpers.masks_to_submission(submission_filename, *image_filenames)
+helpers.masks_to_submission(submission_filename, preds.numpy())
   
   
   
